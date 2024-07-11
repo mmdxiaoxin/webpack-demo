f65faa4f7f14510b41eefc513187a3b589bb327d
haojingxin <haojingxin@meituan.com>
2024-07-11 14:36:09 +0800

    study: 管理输出

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import _ from "lodash";
-import "./style.css"
+import "./styles/style.css"
+import ImageTest from "../assets/image.png"
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
+
+    const testImage = new Image();
+    testImage.src = ImageTest;
   
     // 执行这一行需要引入 lodash（目前通过 script 脚本引入）
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+    element.appendChild(testImage);
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
   },
-  module:{
-    rules:[
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "管理输出",
+    }),
+  ],
+  module: {
+    rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
-}
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
